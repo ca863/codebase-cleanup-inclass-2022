2e560601a776b153ae81ed7fda39cdd0a4a3b6be
ca863 <ca863@georgetown.edu>
2022-04-08 18:53:54 -0400

    If main conditional added to utils

---
+++ app/utils.py
@@ -0,0 +1,23 @@
+def to_usd(my_price):
+    """
+    Converts a numeric value to usd-formatted string, for printing and display purposes.
+    
+    Param: my_price (int or float) like 4000.444444
+    
+    Example: to_usd(4000.444444)
+    
+    Returns: $4,000.44
+    """
+    return '${:,.2f}'.format(my_price)
+
+
+if __name__ == "__main__":
+
+    # nesting code in the main condition will
+    # allow other scripts to cleanly import functions from this file
+    # without running this code
+
+    # this code still gets run when we invoke the script from the command line
+    price = input("Please choose a price like 4.9999")
+
+    print(to_usd(float(price)))
