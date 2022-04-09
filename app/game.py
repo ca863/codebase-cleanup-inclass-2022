422ae3b9aa2fa195885c4073c49078a9f36f6a99
ca863 <ca863@georgetown.edu>
2022-04-08 20:21:32 -0400

    Update game.py

--- app/game.py
+++ app/game.py
@@ -51,25 +51,10 @@ if __name__ == "__main__":
 
     #
     # DETERMINATION OF WINNER
-    #
-
-    if u == "rock" and c == "rock":
-        print("It's a tie!")
-    elif u == "rock" and c == "paper":
-        print("The computer wins")
-    elif u == "rock" and c == "scissors":
-        print("The user wins")
-
-    elif u == "paper" and c == "rock":
-        print("The computer wins")
-    elif u == "paper" and c == "paper":
-        print("It's a tie!")
-    elif u == "paper" and c == "scissors":
-        print("The user wins")
-
-    elif u == "scissors" and c == "rock":
-        print("The computer wins")
-    elif u == "scissors" and c == "paper":
-        print("The user wins")
-    elif u == "scissors" and c == "scissors":
-        print("It's a tie!")
+    winner = determine_winner(u,c)
+    if winner == u:
+        print("YOU WON")
+    elif winner == c:
+        print("COMPUTER WON")
+    else:
+        print("TIE")
