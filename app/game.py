bdac1f3bb94e7cd2019ac5256220526eb22a9f8a
ca863 <ca863@georgetown.edu>
2022-04-08 23:20:28 -0400

    document determine winner

--- app/game.py
+++ app/game.py
@@ -2,31 +2,7 @@
 
 
 from random import choice
-
-
-
-def determine_winner(user_choice, computer_choice):
-    #return "paper"
-    winners = {
-        "rock": {
-            "rock": None,
-            "paper": "paper",
-            "scissors": "rock",
-        },
-        "paper": {
-            "rock": "paper",
-            "paper": None,
-            "scissors": "scissors",
-        },
-        "scissors": {
-            "rock": "rock",
-            "paper": "scissors",
-            "scissors": None,
-        }
-    }
-    winning_choice = winners[user_choice][computer_choice]
-    return winning_choice
-    #return "OOPS"
+from app.utils import determine_winner
 
 
 if __name__ == "__main__":
