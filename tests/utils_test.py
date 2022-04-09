8e638892ec29ae74776b8f8250da95ba97a2b143
ca863 <ca863@georgetown.edu>
2022-04-08 20:07:55 -0400

    Update utils_test.py

--- tests/utils_test.py
+++ tests/utils_test.py
@@ -1,6 +1,7 @@
 
 # this is the "tests/utils_test.py" file...
 
+from tkinter.ttk import Separator
 from app.utils import to_usd
 
 #def test_answer():
@@ -8,4 +9,8 @@ from app.utils import to_usd
 
 
 def test_to_usd():
+    #it rounds to two decimal places (and have a dollar sign):
+    assert to_usd(0.45555) = "$0.46"
+
+    #if large number, should use thousands separator:
     assert to_usd(123456789.98) == "$123,456,789.98"
