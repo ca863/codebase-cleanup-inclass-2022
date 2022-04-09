0610e95213e6d2416754a35b6767ff82cd109d8e
ca863 <ca863@georgetown.edu>
2022-04-08 20:14:13 -0400

    Update utils_test.py

--- tests/utils_test.py
+++ tests/utils_test.py
@@ -10,7 +10,7 @@ from app.utils import to_usd
 
 def test_to_usd():
     #it rounds to two decimal places (and have a dollar sign):
-    assert to_usd(0.45555) = "$0.46"
+    assert to_usd(0.45555) == "$0.46"
 
     #if large number, should use thousands separator:
     assert to_usd(123456789.98) == "$123,456,789.98"
