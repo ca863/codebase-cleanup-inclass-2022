0127d0bd20e1882a453df8c4f0b18056324f4fd1
ca863 <ca863@georgetown.edu>
2022-04-08 23:52:02 -0400

    explain determine_winner function

--- app/utils.py
+++ app/utils.py
@@ -21,3 +21,40 @@ if __name__ == "__main__":
     price = input("Please choose a price like 4.9999")
 
     print(to_usd(float(price)))
+
+
+
+def determine_winner(user_choice, computer_choice):
+
+    """
+    This function takes two strings the computer choice and the user choice. It runs a logical test to determine the winner of the rock paper
+    scissor exercise.... This function will return the winning choice of the game
+    
+    Param: rock_paper_scissor (string,string) like 'rock'
+    
+    Example: determine_winner('rock','scissors')
+    
+    Returns: 'rock'
+    """
+
+    #return "paper"
+    winners = {
+        "rock": {
+            "rock": None,
+            "paper": "paper",
+            "scissors": "rock",
+        },
+        "paper": {
+            "rock": "paper",
+            "paper": None,
+            "scissors": "scissors",
+        },
+        "scissors": {
+            "rock": "rock",
+            "paper": "scissors",
+            "scissors": None,
+        }
+    }
+    winning_choice = winners[user_choice][computer_choice]
+    return winning_choice
+    #return "OOPS"
